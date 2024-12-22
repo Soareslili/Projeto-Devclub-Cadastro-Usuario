a9d49f7cdaad1fc55fed134937ec91f6dae41891
Soareslili <lidianesantossoares2@gmail.com>
2024-12-21 21:57:30 -0300

    atualizando projeto

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,16 +1,66 @@
-import Button from '../../components/Button'
-import TopBackground from '../../components/TopBackground';
+import { useEffect, useState } from "react";
+import api from "../../services/api";
+
+import Button from "../../components/Button";
+import TopBackground from "../../components/TopBackground";
+import { useNavigate } from "react-router-dom";
+
+
+import {
+  Container,
+  ContainerUsers,
+  CardUsers,
+  TrashIcon,
+  Title,
+  AvatarUsers,
+} from "./styles";
+import Trash from "../../assets/trash.svg";
+
 
 function ListUsers() {
+  const [users, setUsers] = useState([]);
+  const navegation = useNavigate()
+
+  useEffect(() => {
+    async function getUsers() {
+      const { data } = await api.get("/usuarios");
+
+      setUsers(data);
+    }
+    getUsers();
+  }, []);
+
+    async function deleteUsers(id){
+      await api.delete(`/usuarios/${id}`)
+
+      const upadatedUsers = users.filter(user => user.id !== id)
+
+      setUsers(upadatedUsers)
+    }
   
-    return (
-    <div>
+  return (
+    <Container>
+      <TopBackground />
+      <Title>Lista de Usuário</Title>
 
-        <TopBackground />
+      <ContainerUsers>
+        {users.map((user) => (
+          <CardUsers key={user.id}>
+            <AvatarUsers
+              src={`https://avatar.iran.liara.run/public?username=${user.id}`}
+            />
+            <div>
+              <h3>{user.name}</h3>
+              <p>{user.age}</p>
+              <p>{user.email}</p>
+            </div>
+            <TrashIcon src={Trash} alt="icone-lixo" onClick={() => deleteUsers(user.id)}/>
+          </CardUsers>
+        ))}
+      </ContainerUsers>
 
-      <h1>Listagem de Usuário</h1>
-      <Button>Voltar</Button>
-    </div>
+      <Button type="button" onClick={() => navegation('/')}>Voltar</Button>
+    </Container>
   );
 }
 
--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL:'http://localhost:3001'
+    baseURL:'http://localhost:3002'
 })
 
 export default api
+
+
+//comando para fazer rodar o código (npm run dev), sempre para e rodar quando for atualizar algo.
